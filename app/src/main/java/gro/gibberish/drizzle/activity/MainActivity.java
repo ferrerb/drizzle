a4126222e505741e48e12e35092494b0012c9dd8
Ben Ferrer <ferrer.benl@gmail.com>
2015-04-01 19:49:53 -0400

    Ignore file with API keys and implement toolbar The api_keys.xml file will contain developer keys for the weather underground site, so these keys should not really be distributed all over.
    The toolbar is used as the new age action bar replacement.

--- app/src/main/java/gro/gibberish/drizzle/activity/MainActivity.java
+++ app/src/main/java/gro/gibberish/drizzle/activity/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,6 +25,11 @@ public class MainActivity extends ActionBarActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
         if (savedInstanceState == null) {
             LocationListFragment f = LocationListFragment.newInstance(null, null);
             FragmentTransaction ft = getFragmentManager().beginTransaction();
