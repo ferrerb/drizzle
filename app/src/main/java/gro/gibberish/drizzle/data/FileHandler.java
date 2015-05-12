048fce5c7f6ab59e0b5e5b3bb6ec36a616e35610
Ben Ferrer <ferrer.benl@gmail.com>
2015-05-12 19:47:43 -0400

    Create an observable for retreiving files There is now a method for returning an observable with the desired object from a file, emitted on subscribe. Also implemented error handling using the observable methods instead of just using println.

--- app/src/main/java/gro/gibberish/drizzle/data/FileHandler.java
+++ app/src/main/java/gro/gibberish/drizzle/data/FileHandler.java
@@ -13,6 +13,7 @@ import java.io.Serializable;
 
 import rx.Observable;
 import rx.Subscriber;
+import rx.exceptions.OnErrorThrowable;
 import rx.schedulers.Schedulers;
 
 /**
@@ -85,13 +86,54 @@ public class FileHandler {
         return data;
     }
 
-    public static <T> Observable<T> getSerializedObjectObservable(Class<T> type, File path, String filename) {
-        return Observable.just(getSerializedObjectFromFile(type, path, filename))
-                .subscribeOn(Schedulers.io());
+    /**
+     * Retrieves a serialized object from file
+     *
+     * @param type The class type to be deserialized
+     * @param path The path to the desired file, uses a File object since getCacheDir() returns one
+     * @param fileName The name of the file to be deserialized
+     * @param <T> The class type to be deserialized
+     * @return An observable which emits an object of type <T> containing created from the specified file
+     */
+    /* The warning is from the cast from the file object, to the desired type. I don't know a good
+     * way to avoid this, and I can't move it to be more localized with the try statement. Perhaps I could
+     * create a separate object in the try block, and then assign it to the data variable. So, make
+     * sure you pass the class which matches the serialized object. No pressure.
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> Observable<T> getSerializedObjectObservable(Class<T> type, File path, String fileName) {
+        return Observable.create(new Observable.OnSubscribe<T>() {
+            @Override
+            public void call(Subscriber<? super T> subscriber) {
+                try {
+                    T data;
+                    FileInputStream fis = new FileInputStream(
+                            new File(path, fileName));
+                    ObjectInputStream in = new ObjectInputStream(fis);
+
+                    data = (T) in.readObject();
+                    in.close();
+
+                    subscriber.onNext(data);
+                    subscriber.onCompleted();
+                }
+                catch (IOException | ClassNotFoundException e) {
+                    throw OnErrorThrowable.from(e);
+                }
+            }
+        }).subscribeOn(Schedulers.io());
     }
 
-    public static <T extends Serializable> void saveSerializedObjectObservable(T data, File path, String fileName) {
-        Observable<Void> o = Observable.create(new Observable.OnSubscribe<Void>() {
+    /**
+     * Serializes an object to a file
+     *
+     * @param data The object to be serialized
+     * @param path The path to the to-be-created file, uses a File object since getCacheDir() returns one
+     * @param fileName The name of the file to be created
+     * @param <T> The type of object being serialized
+     */
+    public static <T extends Serializable> Observable<Void> saveSerializedObjectObservable(T data, File path, String fileName) {
+        return Observable.create(new Observable.OnSubscribe<Void>() {
             @Override
             public void call(Subscriber<? super Void> subscriber) {
                 try {
@@ -103,10 +145,10 @@ public class FileHandler {
                     out.close();
                 }
                 catch (FileNotFoundException e) {
-                    System.err.println("FileNotFoundException " + e.getMessage());
+                    throw OnErrorThrowable.from(e);
                 }
                 catch (IOException e) {
-                    System.err.println("IOException " + e.getMessage());
+                    throw OnErrorThrowable.from(e);
                 }
                 subscriber.onCompleted();
             }
